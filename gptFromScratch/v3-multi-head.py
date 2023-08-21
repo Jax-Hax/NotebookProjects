0bb8c063203bc4e55791a0f975820e5fc195c8a8
Jax-Hax <jaxhacks543@gmail.com>
2023-08-21 09:00:39 -0400

    prescaled model

--- gptFromScratch/v3-multi-head.py
+++ gptFromScratch/v3-multi-head.py
@@ -12,6 +12,7 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print("device: " + device)
 eval_iters = 200
 n_embd = 32 # number of embedding dimensions
+dropout = 0.2
 # ------------
 
 torch.manual_seed(1337)
@@ -91,14 +92,48 @@ class MultiHeadAttention(nn.Module):
     def __init__(self, num_heads, head_size):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
-        #self.proj = nn.Linear(n_embd, n_embd)
-        #self.dropout = nn.Dropout(dropout)
+        self.proj = nn.Linear(head_size * num_heads, n_embd)
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         out = torch.cat([h(x) for h in self.heads], dim=-1)
-        #out = self.dropout(self.proj(out))
+        out = self.dropout(self.proj(out))
         return out
+
     
+
+class FeedFoward(nn.Module):
+    """ a simple linear layer followed by a non-linearity """
+
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd),
+            nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
+            #nn.Dropout(dropout),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+    
+class Block(nn.Module):
+    """ Transformer block: communication followed by computation """
+
+    def __init__(self, n_embd, n_head):
+        # n_embd: embedding dimension, n_head: the number of heads we'd like
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedFoward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
+
 # super simple bigram model
 class BigramLanguageModel(nn.Module):
 
@@ -107,7 +142,11 @@ class BigramLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size,n_embd)
-        self.sa_heads = MultiHeadAttention(4,n_embd//4) # 4 heads of 8 dimensional self attention
+        self.blocks = nn.Sequential(
+            Block(n_embd,n_head=4),
+            Block(n_embd,n_head=4),
+            Block(n_embd,n_head=4),
+        )
         self.lm_head = nn.Linear(n_embd,vocab_size)
 
     def forward(self, idx, targets=None):
@@ -116,7 +155,7 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx) # (B,T,C)
         pos_emb = self.position_embedding_table(torch.arange(T,device=device)) # (T,C)
         x = tok_emb + pos_emb # (B,T,C)
-        x = self.sa_heads(x)
+        x = self.blocks(x)
         logits = self.lm_head(x) # (B,T,vocab_size)
         if targets is None:
             loss = None
